71bb7ae26947f2bbe4868f8f0905e076a55bd929
Ian Ding <17621762731@126.com>
2026-01-23 23:25:29 +0800

    重置代码

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -27,58 +27,17 @@ import Placeholder from "./Placeholder.jsx";
 import Hamburger from "./Hamburger.jsx";
 import Fox from "./Fox.jsx";
 
-// softShadows({
-//   frustum: 3.75,
-//   size: 0.005,
-//   near: 9.5,
-//   samples: 17,
-//   rings: 11,
-// });
-
 export default function Experience() {
-  const directionalLight = useRef();
-  useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
-
   return (
     <>
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
 
-      {/* 灯光系统 */}
-      <directionalLight
-        ref={directionalLight}
-        castShadow
-        position={[1, 2, 3]}
-        intensity={1.5}
-        shadow-normalBias={0.04}
-      />
-      <ambientLight intensity={0.5} />
-
-      {/* 地面平面 */}
-      <mesh
-        receiveShadow
-        position-y={-1}
-        rotation-x={-Math.PI * 0.5}
-        scale={10}
-      >
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+      <mesh scale={1.5}>
+        <boxGeometry />
+        <meshNormalMaterial />
       </mesh>
-
-      <Suspense fallback={<Placeholder position-y={0.5} scale={[2, 3, 2]} />}>
-        {/* <Model /> */}
-        <Hamburger scale={0.35} />
-      </Suspense>
-
-      <Fox />
-
-      {/* 自定义对象 */}
-      {/* <CustomObject /> */}
-
-      {/* 辅助工具（开发时使用） */}
-      {/* <axesHelper args={[5]} /> */}
-      {/* <gridHelper args={[10, 10]} /> */}
     </>
   );
 }
