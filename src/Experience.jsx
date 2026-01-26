c7c10029ac5451650e0128bb84cce292a694be97
Ian Ding <17621762731@126.com>
2026-01-26 22:27:32 +0800

    重置代码

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -35,67 +35,34 @@ import portalVertexShader from "./shaders/portal/vertex.glsl";
 import portalFragmentShader from "./shaders/portal/fragment.glsl";
 
 export default function Experience() {
-  const cube = useRef();
-  const hamburger = useGLTF("./hamburger.glb");
-
-  useFrame((state, delta) => {
-    cube.current.rotation.y += delta * 0.2;
-  });
-
-  const evenetHandler = (event) => {
-    console.log("event");
-
-    cube.current.material.color.set(`hsl(${Math.random() * 360},100%,75%)`);
-  };
-
   return (
     <>
+      <Perf position="top-left" />
+
       <OrbitControls makeDefault />
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh
-        position-x={-2}
-        onClick={(event) => {
-          event.stopPropagation();
-        }}
-      >
+      <mesh castShadow position-x={-2}>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh
-        ref={cube}
-        raycast={meshBounds}
-        position-x={2}
-        scale={1.5}
-        onClick={evenetHandler}
-        onPointerEnter={(event) => {
-          document.body.style.cursor = "pointer";
-        }}
-        onPointerLeave={(event) => {
-          document.body.style.cursor = "default";
-        }}
-      >
+      <mesh castShadow position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
-
-      <primitive
-        object={hamburger.scene}
-        scale={0.25}
-        position-y={0.5}
-        onClick={(event) => {
-          console.log("hamburger", event.object.name, event);
-          event.stopPropagation();
-        }}
-      />
     </>
   );
 }
